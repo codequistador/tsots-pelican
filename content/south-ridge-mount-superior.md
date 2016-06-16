c43394374fc494094bc571d446ca65a390cdd432
Justin Daining <justin@justindaining.com>
2016-06-16 16:57:57 -0600

    gulpfile and content

---
+++ content/south-ridge-mount-superior.md
@@ -0,0 +1,7 @@
+Title: South Ridge. Mount Superior.
+Date: 2016-06-08 10:00
+Summary: LESS THOUGHTS. MORE ACTION.
+
+<div class="iframe_wrap">
+<iframe src="https://player.vimeo.com/video/170814306?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>
--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
 var concat = require('gulp-concat');
 var concatCSS = require('gulp-concat-css');
-var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var run = require('gulp-shell');
 var runSequence = require('run-sequence');
 var rsync  = require('gulp-rsync');
+var del = require('del');
 
 var config = {
 	themeDir: './themes/tsots',
@@ -38,7 +38,15 @@ var config = {
 
 var reload = browserSync.reload;
 
-gulp.task('sass', function() {
+gulp.task('clean:dist', function() {
+  return del.sync(config.cssDir + '/dist/all.css');
+});
+
+gulp.task('clean:output', function() {
+  return del.sync(config.dist);
+});
+
+gulp.task('sass', ['clean:dist'], function() {
   return gulp.src(config.scssDir + '/styles.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest(config.cssDir))
@@ -47,14 +55,13 @@ gulp.task('sass', function() {
 gulp.task('css', ['sass'], function() {
 	return gulp.src(config.cssDir + '/**/*.css')
 	.pipe(concatCSS('all.css', {rebaseUrls: false}))
-  .pipe(cleanCSS())
 	.pipe(gulp.dest(config.cssDir + '/dist'))
 	.pipe(browserSync.reload({
     stream: true
   }));
 });
 
-gulp.task('builddev', ['css'], function() {
+gulp.task('pelicandev', ['css'], function() {
   return gulp.src('')
     .pipe(run('pelican content -s pelicanconf.py'))
     .pipe(browserSync.reload({
@@ -62,12 +69,20 @@ gulp.task('builddev', ['css'], function() {
 	  }));
 });
 
-gulp.task('buildprod', ['css'], function() {
+gulp.task('builddev', function(callback) {
+  runSequence('clean:output', 'pelicandev', callback);
+});
+
+gulp.task('pelicanprod', ['css'], function() {
   return gulp.src('')
     .pipe(run('pelican content -s publishconf.py'))
     .pipe(browserSync.reload({
 	    stream: true
 	  }));
+});
+
+gulp.task('buildprod', function(callback) {
+  runSequence('clean:output', 'pelicanprod')
 });
 
 gulp.task('server', ['builddev'], function() {
@@ -84,7 +99,7 @@ gulp.task('deploy', ['buildprod'], function() {
 });
 
 gulp.task('default', ['server'], function() {
-  gulp.watch(config.scssDir + '/**/*.scss', ['css']);
+  gulp.watch(config.scssDir + '/**/*.scss', ['builddev']);
   gulp.watch(config.themeDir + '/templates/**/*.html', ['builddev']);
   gulp.watch(config.contentDir + '/**/*.md', ['builddev']);
   return
